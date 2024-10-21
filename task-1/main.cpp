107db9a8e0069533619d7d03d7f75b67ce24805a
a.burlakov <a.burlakov@g.nsu.ru>
2024-10-21 09:07:13 +0700

    minor changes

--- task-1/main.cpp
+++ task-1/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include "src/BitArray.h"
 
 using namespace std;
 
 int main() {
-    
+    cout << 1;
     return 0;
 }
--- task-1/src/BitArray.h
+++ task-1/src/BitArray.h
@@ -1,5 +1,12 @@
 #pragma once
 
 class BitArray {
-
+    private:
+        char data;
+    public:
+        class Wrapper {
+            private:
+                BitArray *link;
+                int index;
+        };
 };
